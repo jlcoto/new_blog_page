6b985932becf01377f966da832b4117194531ed7
jlcoto <jscoto@gmail.com>
2017-08-26 12:31:00 +0200

    Fixes hamburger menu bug.

--- js/responsive.js
+++ js/responsive.js
@@ -1,3 +1,15 @@
 $(".nav-menu").click(function(){
-	$(".nav-list").slideToggle()
-})
+	$(".nav-list").slideToggle();
+	$("#sky-blue-triangle").slideToggle();
+});
+
+
+$(window).on('resize', function(){
+	if($(window).width() > 768) {
+		$("#sky-blue-triangle").css('display', 'none');
+		$(".nav-list").show();
+	} else if (($(window).width() < 768) && ($("#sky-blue-triangle").css('display') == 'none')) {
+			$(".nav-list").hide();
+		}
+	}
+)
