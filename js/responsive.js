9a4eb514f45b9230ea1b0123af2fed8bfe0f3783
jlcoto <jscoto@gmail.com>
2017-09-12 15:49:26 +0200

    Use jquery to show projects.

--- js/responsive.js
+++ js/responsive.js
@@ -32,6 +32,45 @@ $(function() {
 });
 
 
+// Show selected Projects
+
+var projects = [
+	{
+		title: "A/B Testing",
+		description: ["Designed an A/B test, including which metrics to measure and how long the test should be run.",
+					"Analyzed the results of an A/B test that was ran by Udacity.",
+					"Recommended a decision, and proposed a follow-up experiment."],
+		skills: "Pandas, Matplotlib, Jupyter Notebook."
+	},
+];
+
+
+
+var projectList = ''
+
+projects.forEach(function (entry) {
+
+	var projDescription = ''
+	entry.description.forEach(function(pointer) {
+		projDescription += "<li>" + pointer + "</li>"
+	});
+
+	projectList += "<div class='proj-content'> \
+		<div class='proj-background'> \
+		<div class='title-pos text-right'> \
+			<span class='proj-title'>" + entry.title + "</span> \
+		</div> \
+		<div class='proj-description'> \
+			<ul class='proj-bullet-description'>" + projDescription + "</ul>"
+			+ entry.skills +
+		"</div> \
+		</div>"
+})
+
+$(".projects").append(projectList);
+
+
+
 // Showing project descriptions
 
 $(".proj-content").mouseover(function(){
