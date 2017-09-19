0ba9c3a9ea1456484f7abd6bc7f12e567dbc1021
jlcoto <jscoto@gmail.com>
2017-09-19 23:56:37 +0200

    Use jquery to show all the projects.

--- js/responsive.js
+++ js/responsive.js
@@ -39,7 +39,7 @@ var projects = [
 		id: "testing",
 		link: "https://github.com/jlcoto/Udacity/tree/master/a_b_testing",
 		title: "A/B Testing",
-		image: "url(/img-layout/a_b_testing_background.png)",
+		image: "/img-layout/a_b_testing_background.png",
 		description: ["Designed an A/B test, including which metrics to measure and how long the test should be run.",
 					"Analyzed the results of an A/B test that was ran by Udacity.",
 					"Recommended a decision, and proposed a follow-up experiment."],
@@ -48,7 +48,7 @@ var projects = [
 		id: "earthquake",
 		link: "https://github.com/jlcoto/Udacity/tree/master/earthquake_visualization",
 		title: "Earthquakes visualization",
-		image: "url(/img-layout/earthquake_plot.png)",
+		image: "/img-layout/earthquake_plot.png",
 		description: ["Developed visualization where users can fully interact with geographical and temporal features of earthquakes.",
 					"Successfully integrated D3.js and Leaflet to produce animations and transitions.",
 					"Project featured by Data Science Weekly."],
@@ -58,7 +58,7 @@ var projects = [
 		id: "machine-learning",
 		link: "https://github.com/jlcoto/Udacity/tree/master/machine_learning_project",
 		title: "Machine Learning",
-		image: "url(/img-layout/machine_learning.png)",
+		image: "/img-layout/machine_learning.png",
 		description: ["Identified which Enron employees are more likely to have committed fraud using machine learning and public Enron financial and email data.",
 					"Trained and tested different algorithms and used feature selection techniques.",
 					"Tunned algorithms’ parameters to improve original results."],
@@ -68,7 +68,7 @@ var projects = [
 		id: "exploratory-r",
 		link: "https://github.com/jlcoto/Udacity/tree/master/earthquake_project",
 		title: "Exploratory Analysis R",
-		image: "url(/img-layout/exploratory_r.png)",
+		image: "/img-layout/exploratory_r.png",
 		description: ["Cleaned, merged and analyzed data on consequences of earthquake in the world from the 1900s.",
 					"Created a notebook with clear steps for getting, cleaning and merging the data from different sources. Created a codebook with all the variables included in final dataset.",
 					"Created more than 20 visualizations to understand the data. Analyzed the conditional relationships of deaths of earthquakes given its magnitude and regime type/gdp per capita."],
@@ -78,7 +78,7 @@ var projects = [
 		id: "data-wrangling",
 		link: "https://github.com/jlcoto/Udacity/tree/master/data_wrangling/Open_Street_project/Open_street_lima_project",
 		title: "Data Wrangling SQL and Pandas",
-		image: "url(/img-layout/wrangling_data.png)",
+		image: "/img-layout/wrangling_data.png",
 		description: ["Parsed 140 Mb XML document to obtain relevant data.",
 					"Cleaned, audited and corrected more than 2500 registries.",
 					"Stored cleaned data in a SQL database, performed queries and generated plots. Created map plots to inspect georeferenced data."],
@@ -88,7 +88,7 @@ var projects = [
 		id: "exploratory-pandas",
 		link: "https://github.com/jlcoto/Udacity/tree/master/pandas_wrangling",
 		title: "Exploratory Analysis Pandas",
-		image: "url(/img-layout/exploratory_analysis_pandas.png)",
+		image: "/img-layout/exploratory_analysis_pandas.png",
 		description: ["Analyzed Titanic Data.",
 					"Studied relationships between variables and created visualizations.",
 					"Answered questions about the variables related to surviving the Titanic."],
@@ -98,7 +98,7 @@ var projects = [
 		id: "stats-python",
 		link: "https://github.com/jlcoto/Udacity/tree/master/statistics/stroop_task",
 		title: "Statistical Analysis with Python",
-		image: "url(/img-layout/stats_python.png)",
+		image: "/img-layout/stats_python.png",
 		description: ["Analyzed validity of stroop effect.",
 					"Conducted exploratory analysis.",
 					"Used different visualizations to understand the stroop effect."],
@@ -107,45 +107,114 @@ var projects = [
 ];
 
 
-
 var projectList = ''
 
-projects.forEach(function (entry) {
 
+projects.forEach(function (entry, index) {
 
 	var projDescription = ''
 	entry.description.forEach(function(pointer) {
 		projDescription += "<li>" + pointer + "</li>"
 	});
 
-	$(".projects").append("<div class='proj-content'> <a href=" + entry.link +" target='#'> \
-		<div class='proj-background' id="+entry.id+ "> \
-		<div class='title-pos text-right'> \
-			<span class='proj-title'>" + entry.title + "</span> \
-		</div> \
-		<div class='proj-description'> \
-			<ul class='proj-bullet-description'>" + projDescription + "</ul> <span class='skills'> Skills: "
-			+ entry.skills + "</span> \
-		</div> \
-		</a></div>")
-
-	$("#" +entry.id ).css("background", entry.image)
+	if (index % 2 == 0) {
+		projectList += "<div class='row'> \
+			<div class='col-md-5 project'> \
+				<div class='proj-title'>" + entry.title + "</div> \
+			<div class='row'> \
+			<div class='col-md-6 text-center'> \
+				<img class='img-fluid' src=" + entry.image + ">\
+			</div> \
+			<div class='col-md-6 proj-content'> \
+				<ul class='proj-description proj-bullet-description'>"
+					+ projDescription +
+				"</ul>\
+				<div class='skills'> Skills:" + entry.skills +"</div>\
+			</div>\
+			</div> \
+			</div>"
+	}  else {
+		projectList += "<div class='col-md-1'> \
+			</div> \
+		<div class='col-md-5 project'> \
+			<div class='proj-title'>" + entry.title + "</div> \
+			<div class='row'> \
+			<div class='col-md-6 text-center'> \
+				<img class='img-fluid' src=" + entry.image + ">\
+			</div> \
+			<div class='col-md-6 proj-content'> \
+				<ul class='proj-description proj-bullet-description'>"
+					+ projDescription +
+				"</ul>\
+				<div class='skills'> Skills:" + entry.skills +"</div>\
+			</div> \
+			</div> \
+			</div> \
+			</div>"
+	}
 })
 
+$(".projects-show").after(projectList);
+
+
+// <div class="row">
+// 	<div class="col-md-5 project">
+// 		<div class='proj-title'> Earthquake </div>
+// 		<div class="row">
+// 			<div class="col-md-6 text-center">
+// 				<img class="img-fluid" src="/img-layout/earthquake_plot.png">
+// 			</div>
+// 			<div class="col-md-6 proj-content">
+// 				<ul class="proj-description proj-bullet-description">
+// 					<li>Developed visualization where users can fully interact with geographical and temporal features of earthquakes.</li>
+// 					<li>Successfully integrated D3.js and Leaflet to produce animations and transitions.</li>
+// 					<li>Project featured by Data Science Weekly.</li>
+// 				</ul>
+// 				<div class="skills"> Skills: D3.js, Leaflet, GeoPandas, Pandas, Python. </div>
+// 			</div>
+// 		</div>
+// 	</div>
 
 
 
-// Showing project descriptions
+// var projectList = ''
 
-$(".proj-content").mouseover(function(e){
-	$(this).find(".proj-title").css({"margin-top": "30px"});
-	$(this).find(".proj-description").fadeIn();
-})
+// projects.forEach(function (entry) {
 
-$(".proj-content").mouseleave(function(e){
-	$(".proj-description").hide();
-	$(".proj-title").css({"margin-top": "250px"});
-})
+
+// 	var projDescription = ''
+// 	entry.description.forEach(function(pointer) {
+// 		projDescription += "<li>" + pointer + "</li>"
+// 	});
+
+// 	$(".projects").append("<div class='proj-content'> <a href=" + entry.link +" target='#'> \
+// 		<div class='proj-background' id="+entry.id+ "> \
+// 		<div class='title-pos text-right'> \
+// 			<span class='proj-title'>" + entry.title + "</span> \
+// 		</div> \
+// 		<div class='proj-description'> \
+// 			<ul class='proj-bullet-description'>" + projDescription + "</ul> <span class='skills'> Skills: "
+// 			+ entry.skills + "</span> \
+// 		</div> \
+// 		</a></div>")
+
+// 	$("#" +entry.id ).css("background", entry.image)
+// })
+
+
+
+
+// // Showing project descriptions
+
+// $(".proj-content").mouseover(function(e){
+// 	$(this).find(".proj-title").css({"margin-top": "30px"});
+// 	$(this).find(".proj-description").fadeIn();
+// })
+
+// $(".proj-content").mouseleave(function(e){
+// 	$(".proj-description").hide();
+// 	$(".proj-title").css({"margin-top": "250px"});
+// })
 
 
 
